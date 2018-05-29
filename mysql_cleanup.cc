9bfeddeb9e1cbb8dc01f32c2403bb07ab6857a26
LittlePox <e0005280@u.nus.edu>
2018-05-30 00:04:56 +0800

    loop and delete files

--- mysql_cleanup.cc
+++ mysql_cleanup.cc
@@ -1,46 +1,67 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
-
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/filesystem.hpp>
 using namespace std;
 
 const char* unwanted_token[] = {"\n", "DROP", "LOCK", "UNLOCK", "--", "/*!"};
 
+vector<string> find_all_sql();
+
 int main(int argc, char** argv)
 {
-    if (argc < 2)
+    vector<string> all_sql = find_all_sql();
+    for (string &file : all_sql)
     {
-        fprintf(stderr, "%s", "No input file.");
-        return 0;
-    }
-    if (strstr(argv[1], "_") == NULL)
-    {
-        fprintf(stderr, "%s", "File name does not contain \'_\'.");
-        return 0;
-    }
-    FILE *fin = fopen(argv[1], "r");
-    char *outfile = strstr(argv[1], "_") + 1;
-    FILE *fout = fopen(outfile, "w");
-    char line[1000000];
-    while (fgets(line, 1000000, fin) != NULL)
-    {
-        bool unwanted = false;
-        for (char *token : unwanted_token)
+        const char *infile = file.c_str();
+        FILE *fin = fopen(infile, "r");
+        const char *outfile = strstr(infile, "_") + 1;
+        FILE *fout = fopen(outfile, "w");
+        char line[1000000];
+        while (fgets(line, 1000000, fin) != NULL)
         {
-            if (strstr(line, token) == line)
+            bool unwanted = false;
+            for (const char *token : unwanted_token)
             {
-                unwanted = true;
-                break;
+                if (strstr(line, token) == line)
+                {
+                    unwanted = true;
+                    break;
+                }
             }
+            if (unwanted)
+            {
+                continue;
+            }
+            fprintf(fout, "%s", line);
         }
-        if (unwanted)
-        {
-            continue;
-        }
-        fprintf(fout, "%s", line);
+        fclose(fin);
+        fclose(fout);
+        remove(infile);
     }
-    fclose(fin);
-    fclose(fout);
     return 0;
 }
+
+vector<string> find_all_sql()
+{
+    vector<string> res;
+    namespace fs = boost::filesystem;
+    fs::path full_path(fs::initial_path<fs::path>());
+    fs::directory_iterator end_itr;
+    for (fs::directory_iterator item(full_path); item != end_itr; item ++) {
+        if (fs::is_regular_file(item->status())) {
+            wstring filename_w = item->path().filename().native();
+            string filename = string(filename_w.begin(), filename_w.end());
+            if (filename.find("_")!= string::npos && filename.find(".sql") != string::npos)
+            {
+                res.push_back(filename);
+            }
+        }
+    }
+    return res;
+}
+
 
